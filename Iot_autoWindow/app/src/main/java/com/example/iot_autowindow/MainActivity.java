27eb2d36e914ec363666736422de739af50047d5
kenny8062 <kenny8062@naver.com>
2020-12-01 16:49:38 +0900

    s3 link

--- Iot_autoWindow/app/src/main/java/com/example/iot_autowindow/MainActivity.java
+++ Iot_autoWindow/app/src/main/java/com/example/iot_autowindow/MainActivity.java
@@ -23,14 +23,23 @@ import com.amazonaws.services.s3.model.CannedAccessControlList;
 import com.google.firebase.iid.FirebaseInstanceId;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
-    Context context=this;
+
+    TextView text_temp;
+    TextView text_humid;
+    TextView text_window;
+    Button button_open;
+    Button button_close;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -46,27 +55,34 @@ public class MainActivity extends AppCompatActivity {
             }
             else{
                 String token = task.getResult().getToken();
+                Log.w("FirebaseSettingEx", token);
                 System.out.println(token);
             }
         });
-        TextView text_temp=findViewById(R.id.textView);
-        TextView text_humid=findViewById(R.id.textView2);
-        TextView text_window=findViewById(R.id.textView3);
+
+        downloadWithTransferUtility();
         Button button_open=findViewById(R.id.button2);
         Button button_close=findViewById(R.id.button);
-        text_temp.setText("온도 : " + temp);
-        text_humid.setText("습도 : " + humid);
-        text_window.setText("창문 : " +Window);
         button_open.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v) {
-                downloadWithTransferUtility();
+                try {
+                    uploadWithTransferUtility("열림");
+                    downloadWithTransferUtility();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
             }
         });
         button_close.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v) {
-
+                try {
+                    uploadWithTransferUtility("닫힘");
+                    downloadWithTransferUtility();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
             }
         });
 
@@ -86,8 +102,8 @@ public class MainActivity extends AppCompatActivity {
                         .s3Client(new AmazonS3Client(credentialsProvider, Region.getRegion(Regions.AP_NORTHEAST_2)))
                         .build();
 
-        File file = new File(getApplicationContext().getFilesDir(), "/test.txt");
-        String string="test.txt";
+        File file = new File(getFilesDir(),"text.txt");
+        String string="text.txt";
         TransferObserver downloadObserver =
                 transferUtility.download(
                         string,
@@ -105,6 +121,12 @@ public class MainActivity extends AppCompatActivity {
                     for(int i=0;i<tv.size();i++){
                         System.out.println(tv.get(i));
                     }
+                    text_temp=findViewById(R.id.textView);
+                    text_humid=findViewById(R.id.textView2);
+                    text_window=findViewById(R.id.textView3);
+                    text_temp.setText(tv.get(0));
+                    text_humid.setText(tv.get(1));
+                    text_window.setText(tv.get(2));
                 }catch (FileNotFoundException e){
                     e.printStackTrace();
                 }catch (IOException e) {
@@ -123,7 +145,24 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
-    public void uploadWithTransferUtility(String filename,File file){
+    public void uploadWithTransferUtility(String str) throws IOException {
+        String filename="text.txt";
+        File file = new File(getFilesDir(),"text.txt");
+        BufferedReader br = new BufferedReader(new InputStreamReader(new FileInputStream(file)));
+        String line;
+        int position=2;
+        String dummy="";
+        for(int i=0; i<position; i++) {
+            line = br.readLine(); //읽으며 이동
+            dummy += (line + "\r\n" );
+        }
+        line=br.readLine();
+        if(str=="열림") dummy+="닫힘" +"\r\n";
+        else dummy +="열림" +"\r\n";
+        FileWriter fw = new FileWriter(filename);
+        fw.write(dummy);
+        br.close();
+        fw.close() ;
         CognitoCachingCredentialsProvider credentialsProvider = new CognitoCachingCredentialsProvider(
                 getApplicationContext(),
                 "ap-northeast-2:f80fe4d0-e4d3-4e09-be94-f5d452722201", // 자격 증명 풀 ID
